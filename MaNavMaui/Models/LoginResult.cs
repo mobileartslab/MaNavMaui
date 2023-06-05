f941efaf6adcace3059eb21228fef997ca62d2be
Nick Sophinos <nick@wdeanmedical.com>
2023-06-04 21:28:11 -0400

    login working

---
+++ MaNavMaui/Models/LoginResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MaNavMaui.Models;
+using Newtonsoft.Json;
+
+namespace MaNavMaui.Models
+{
+  public class LoginResult
+  {
+    public LoginResult()
+    {
+    }
+
+    public string error { get; set; }
+    [JsonProperty("result")]
+    public LoginUser user { get; set; }
+  }
+}
+
--- MaNavMaui/Platforms/Android/MainApplication.cs
+++ MaNavMaui/Platforms/Android/MainApplication.cs
@@ -3,7 +3,7 @@ using Android.Runtime;
 
 namespace MaNavMaui;
 
-[Application]
+[Application(UsesCleartextTraffic = true)]
 public class MainApplication : MauiApplication
 {
 	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
